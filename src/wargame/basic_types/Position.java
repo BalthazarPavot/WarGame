f2372cd7a5fe4176c0b391ca629d4cdad4af29bd
balto <balto@lain>
2016-11-09 23:19:54 +0100

    Position can give the distance between two positions and give the list of neighbor positions.

--- src/wargame/basic_types/Position.java
+++ src/wargame/basic_types/Position.java
@@ -2,6 +2,9 @@
 
 package wargame.basic_types ;
 
+import java.util.ArrayList;
+
+import wargame.map.Map;
 
 public class Position {
 
@@ -27,6 +30,36 @@ public class Position {
 
 	public int getY () {
 		return y ;
+	}
+
+	public double distance (int x, int y) {
+		return Math.sqrt (Math.pow ((double)(x - this.x), (double)2) +
+				Math.pow ((double) (y - this.y), (double)2)) ;
+	}
+
+	public double distance (Position position) {
+		return distance (position.getX (), position.getY ()) ;
+	}
+
+	public boolean equals (Object o) {
+		return o.getClass () == this.getClass () &&
+				this.x == ((Position)o).getX () &&
+				this.y == ((Position)o).getY () ;
+	}
+
+	public ArrayList<Position> getNeighbor () {
+		ArrayList<Position> neighbor ;
+
+		neighbor = new ArrayList<Position> () ;
+		neighbor.add(new Position (x, y-Map.squareHeight)) ;
+		neighbor.add(new Position (x+Map.squareWidth, y-Map.squareHeight)) ;
+		neighbor.add(new Position (x+Map.squareWidth, y)) ;
+		neighbor.add(new Position (x+Map.squareWidth, y+Map.squareHeight)) ;
+		neighbor.add(new Position (x, y+Map.squareHeight)) ;
+		neighbor.add(new Position (x-Map.squareWidth, y+Map.squareHeight)) ;
+		neighbor.add(new Position (x-Map.squareWidth, y)) ;
+		neighbor.add(new Position (x-Map.squareWidth, y-Map.squareHeight)) ;
+		return neighbor ;
 	}
 
 }
