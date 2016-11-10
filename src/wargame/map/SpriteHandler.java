f24cea2c9aa810d1c6713d78aaf9c513abbacea7
balto <balto@lain>
2016-11-10 03:58:12 +0100

    Got some images and references.

---
+++ src/wargame/map/SpriteHandler.java
@@ -0,0 +1,30 @@
+
+package wargame.map ;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SpriteHandler extends HashMap<String, ArrayList<BufferedImage>> {
+
+	private static final long serialVersionUID = 1L;
+	//http://gamedev.stackexchange.com/questions/53705/how-can-i-make-a-sprite-sheet-based-animation-system
+	private final static String spriteIndex = "resources/spriteIndex.data" ;
+
+	private boolean loaded = false ;
+
+	public SpriteHandler () {
+		
+	}
+
+	public void loadSprites () {
+		if (loaded == false) {
+			loaded = true ;
+			loadSprites (spriteIndex) ;
+		}
+	}
+
+	private void loadSprites (String indexPath) {
+		
+	}
+}
