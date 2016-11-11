8d58be9e4073f4c33e70e0305d612f9becb0f3fa
balto <balto@lain>
2016-11-11 22:38:48 +0100

    Creation of the spot class, used by the shape generator.

---
+++ src/wargame/map/Spot.java
@@ -0,0 +1,26 @@
+
+
+package wargame.map ;
+
+import java.awt.Dimension;
+
+import wargame.basic_types.Position;
+
+public class Spot {
+
+	private Position position ;
+	private Dimension dimensions ;
+
+	public Spot (int x, int y, int w, int h) {
+		position = new Position (x, y) ;
+		dimensions = new Dimension (w, h) ;
+ 	}
+
+	public Position getPosition () {
+		return this.position ;
+	}
+
+	public Dimension getDimension () {
+		return this.dimensions ;
+	}
+}
