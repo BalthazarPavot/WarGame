d5c2de8e82ff5edf9a7c30adc4994ab13f480492
balto <balto@lain>
2016-11-11 22:57:11 +0100

    Added the other screens to screen managers.

--- src/wargame/GameScreenGenerator.java
+++ src/wargame/GameScreenGenerator.java
@@ -1,54 +1,67 @@
 
-package wargame ;
+package wargame;
 
 import wargame.screens.ConfigScreen;
 import wargame.screens.GameScreen;
+import wargame.screens.LoadGameScreen;
 import wargame.screens.MainScreen;
 import wargame.screens.NewGameScreen;
+import wargame.screens.QuickGameScreen;
 
 public class GameScreenGenerator {
 
-  private GameContext gameContext = null ;
-  private GameScreen preparedGameScreen = null ;
+	private GameContext gameContext = null;
+	private GameScreen preparedGameScreen = null;
 
-  public GameScreenGenerator (GameContext gameContext) {
-    this.gameContext = gameContext ;
-  }
+	public GameScreenGenerator(GameContext gameContext) {
+		this.gameContext = gameContext;
+	}
 
-  /**
-   * Create a GameScreen instance and make it ready to run in the game context.
-   *  Store the game screen in preparedGameScreen.
-   * @param gameScreenID   The id of the game screen to prepare.
-   */
-  public void prepareGameScreen (int gameScreenID) {
-    switch (gameScreenID) {
-      case GameScreen.MAIN_MENU_SCREEN:
-        preparedGameScreen = new MainScreen (gameContext) ;
-        break ;
-      case GameScreen.NEW_GAME_SCREEN:
-          preparedGameScreen = new NewGameScreen (gameContext) ;
-          break ;
-      case GameScreen.CONFIGURATION_SCREEN:
-          preparedGameScreen = new ConfigScreen(gameContext) ;
-          break ;
-      default:
-        this.gameContext.getErrorManager ().exitError (String.format (
-          "Unknown screen id: %d", gameScreenID)) ;
-        break ;
-    }
-    preparedGameScreen.prepare () ;
-  }
+	/**
+	 * Create a GameScreen instance and make it ready to run in the game context. Store the game screen in
+	 * preparedGameScreen.
+	 * 
+	 * @param gameScreenID
+	 *            The id of the game screen to prepare.
+	 */
+	public void prepareGameScreen(int gameScreenID) {
+		switch (gameScreenID) {
+		case GameScreen.MAIN_MENU_SCREEN:
+			preparedGameScreen = new MainScreen(gameContext);
+			break;
+		case GameScreen.NEW_GAME_SCREEN:
+			preparedGameScreen = new NewGameScreen(gameContext);
+			break;
+		case GameScreen.LOAD_GAME_SCREEN:
+			preparedGameScreen = new LoadGameScreen(gameContext);
+			break;
+		case GameScreen.QUICK_GAME_SCREEN:
+			preparedGameScreen = new QuickGameScreen(gameContext);
+			break;
+		case GameScreen.CONFIGURATION_SCREEN:
+			preparedGameScreen = new ConfigScreen(gameContext);
+			break;
+		default:
+			this.gameContext.getErrorManager()
+					.exitError(String.format("Unknown screen id: %d", gameScreenID));
+			break;
+		}
+		preparedGameScreen.prepare();
+	}
 
-  /**
-   * @return GameScreen he game screen previously prepared, or null.
-   */ 
-  public GameScreen getGameScreen () {
-    return preparedGameScreen ;
-  }
+	/**
+	 * @return GameScreen he game screen previously prepared, or null.
+	 */
+	public GameScreen getGameScreen() {
+		return preparedGameScreen;
+	}
 
-  /**
-   * Call the good screen initializer in function of the game screen id
-   * @param gameScreenID  The id of the screen to display.
-   * @param preparedGameScreen The screen partialy prepared.
-   */
+	/**
+	 * Call the good screen initializer in function of the game screen id
+	 * 
+	 * @param gameScreenID
+	 *            The id of the screen to display.
+	 * @param preparedGameScreen
+	 *            The screen partialy prepared.
+	 */
 }
