38f057a071be5b2d27e10c53233103d5a2220168
balto <balto@lain>
2016-11-19 14:55:50 +0100

    We can generate spots the does not overlap.

--- src/wargame/map/Shape.java
+++ src/wargame/map/Shape.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 /**
  * Represents a shape. Any shape, randomly generated using the parameters.
+ * 
  * @author Balthazar Pavot
  *
  */
@@ -36,8 +37,8 @@ public abstract class Shape {
 		return spotNumber;
 	}
 
-	public ArrayList<Spot> getSpots () {
-		return this.spots ;
+	public ArrayList<Spot> getSpots() {
+		return this.spots;
 	}
 
 	/**
@@ -60,74 +61,74 @@ public abstract class Shape {
 
 	/**
 	 * Generate the position of each spot of the shape.
+	 * 
 	 * @param ratio
 	 */
 	protected void generateSpots(double ratio) {
-		/*
-		int spotNumber;
+		generateSpots(ratio, false);
+	}
 
-		this.spotNumber = (int) (parameters.getSquareNumber() * ratio / spotSurface);
-		for (spotNumber = 0; spotNumber < this.spotNumber; spotNumber++) {
-			spots.add(new Spot(
-					rangeInt(0, (int) parameters.getDimensions().getWidth()) / spotSurface * spotSurface,
-					rangeInt(0, (int) parameters.getDimensions().getHeight()) / spotSurface * spotSurface,
-					(int) (this.spotSurface * rangeDouble(1 / this.spotSurfaceError, this.spotSurfaceError))
-							/ spotSurface * spotSurface,
-					(int) (this.spotSurface * rangeDouble(1 / this.spotSurfaceError, this.spotSurfaceError))
-							/ spotSurface * spotSurface));
-		}
-		*/
-		int w ;
-		int h ;
-		w = (int)Math.sqrt(this.spotSurface * spotSurfaceError) ;
-		h = (int)Math.sqrt(this.spotSurface * spotSurfaceError) ;
-		/*spots.add (new Spot (rangeInt(0, (int) parameters.getDimensions().getWidth()) / Map.squareWidth*Map.squareWidth,
-				rangeInt(0, (int) parameters.getDimensions().getHeight()) / Map.squareHeight * Map.squareHeight,
-				w, h)) ;
-		*/
-		int beginX = rangeInt(0, (int) parameters.getDimensions().getWidth()) / Map.squareWidth*Map.squareWidth ;
-		int beginY = rangeInt(0, (int) parameters.getDimensions().getHeight()) / Map.squareHeight * Map.squareHeight ;
-		System.out.printf("Generation of spot %d;%d -> %d;%d\n", beginX, beginY, w, h) ;
-		for (int x=beginX;x<beginX+w*Map.squareWidth;x+=Map.squareWidth)
-			for (int y=beginY;y<beginY+w*Map.squareHeight;y+=Map.squareHeight)
+	/**
+	 * Generate the position of each spot of the shape.
+	 * 
+	 * @param ratio
+	 */
+	protected void generateSpots(double ratio, boolean noOverlap) {
+		int w;
+		int h;
+		w = (int) Math.sqrt(this.spotSurface * spotSurfaceError);
+		h = (int) Math.sqrt(this.spotSurface * spotSurfaceError);
+		int beginX = rangeInt(0, (int) parameters.getDimensions().getWidth()) / Map.squareWidth
+				* Map.squareWidth;
+		int beginY = rangeInt(0, (int) parameters.getDimensions().getHeight()) / Map.squareHeight
+				* Map.squareHeight;
+		if (noOverlap)
+			for (int x = beginX; x < beginX + w * Map.squareWidth; x += Map.squareWidth)
+				for (int y = beginY; y < beginY + h * Map.squareHeight; y += Map.squareHeight)
+					for (int dw = -1; dw < 2; dw += 1)
+						for (int dh = -1; dh < 2; dh += 1)
+							if (spots.contains(new Spot(x + dw * Map.squareWidth, y + dh * Map.squareHeight,
+									Map.squareWidth, Map.squareHeight)))
+								return;
+		for (int x = beginX; x < beginX + w * Map.squareWidth; x += Map.squareWidth)
+			for (int y = beginY; y < beginY + h * Map.squareHeight; y += Map.squareHeight)
 				try {
-				spots.add(new Spot (x, y, Map.squareWidth, Map.squareHeight)) ;
+					spots.add(new Spot(x, y, Map.squareWidth, Map.squareHeight));
 				} catch (IndexOutOfBoundsException e) {
 					/**
 					 * The resource is out of the map, ignore it.
 					 */
 				}
-			
-		spotNumber = w * h ;
-		System.out.printf("generated: %d spots\n",  spotNumber) ;
-//		for (int x = 0 ; x < Math.sqrt(this.spotSurface * spotSurfaceError); x++) {
-//			for (int y = 0 ; y < Math.sqrt(this.spotSurface * spotSurfaceError); y++) {
-//				spots.add(new Spot(x, y
-//			}
-//		}
+
+		spotNumber = w * h;
+		// for (int x = 0 ; x < Math.sqrt(this.spotSurface * spotSurfaceError); x++) {
+		// for (int y = 0 ; y < Math.sqrt(this.spotSurface * spotSurfaceError); y++) {
+		// spots.add(new Spot(x, y
+		// }
+		// }
 	}
 
-@SuppressWarnings("unchecked")
-public void distord() {
-	Random rand;
-	int minx;
-	int miny;
-	int maxx;
-	int maxy;
-	ArrayList<Spot> originalList;
+	@SuppressWarnings("unchecked")
+	public void distord() {
+		Random rand;
+		int minx;
+		int miny;
+		int maxx;
+		int maxy;
+		ArrayList<Spot> originalList;
 
-	originalList = (ArrayList<Spot>) spots.clone();
-	rand = new Random();
-	minx = spots.get(0).getPosition().getX();
-	miny = spots.get(0).getPosition().getY();
-	maxx = spots.get(spots.size() - 1).getPosition().getX();
-	maxy = spots.get(spots.size() - 1).getPosition().getY();
-	for (Spot spot : originalList) {
-		if (spot.getPosition().getX() == minx || spot.getPosition().getY() == miny
-				|| spot.getPosition().getY() == maxx || spot.getPosition().getY() == maxy)
-			if (rand.nextInt(3) == 0)
-				spots.remove(spot);
+		originalList = (ArrayList<Spot>) spots.clone();
+		rand = new Random();
+		minx = spots.get(0).getPosition().getX();
+		miny = spots.get(0).getPosition().getY();
+		maxx = spots.get(spots.size() - 1).getPosition().getX();
+		maxy = spots.get(spots.size() - 1).getPosition().getY();
+		for (Spot spot : originalList) {
+			if (spot.getPosition().getX() == minx || spot.getPosition().getY() == miny
+					|| spot.getPosition().getY() == maxx || spot.getPosition().getY() == maxy)
+				if (rand.nextInt(3) == 0)
+					spots.remove(spot);
+		}
 	}
-}
 
 }
