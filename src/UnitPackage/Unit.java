57ba23d0557bf4af18eeab0f0d5bcb46435b8598
slarzack <bennacer.kamil@gmail.com>
2016-11-23 10:12:42 +0100

    rajout du boolein pour savoir si une unite est active pour les action ou non

--- src/UnitPackage/Unit.java
+++ src/UnitPackage/Unit.java
@@ -3,6 +3,15 @@ package UnitPackage;
 public abstract class Unit implements IUnit {
 	
 	protected  Caracteristique maCara;
+	protected boolean isActive;
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
 
 	public  Caracteristique getMaCara() {
 		return maCara;
