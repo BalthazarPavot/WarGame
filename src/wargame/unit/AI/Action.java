5049b7c287e4c3a7d66df45faf72123bb9707ab9
tilito <pelegrinromain@gmail.com>
2016-11-26 23:54:30 +0100

    add the class Action

---
+++ src/wargame/unit/AI/Action.java
@@ -0,0 +1,13 @@
+package wargame.unit.AI;
+
+import wargame.basic_types.Position;
+
+public class Action {
+
+    public enum operation { ATTACK, HEAL, WAIT, FIRE }
+
+    private Position  positionFinal ;
+    private Position  targetPosition ;
+    private operation ope ;
+	
+}
