498890d65054367ab77fdf29b67cbb2868d53037
tilito <pelegrinromain@gmail.com>
2016-11-27 04:33:37 +0100

    add a new class, it represent a cluster of unit

---
+++ src/wargame/unit/AI/Squad.java
@@ -0,0 +1,48 @@
+package wargame.unit.AI;
+
+import java.util.ArrayList;
+
+import wargame.basic_types.Position;
+import wargame.unit.Unit;
+
+public class Squad {
+
+	ArrayList<Unit> unitList;
+	Position center;
+
+	Squad(Unit u1, Unit u2) {
+		ArrayList<Unit> unitList = new ArrayList<Unit>();
+		unitList.add(u1);
+		unitList.add(u2);
+		u1.ai.setSquad(this);
+		u2.ai.setSquad(this);
+		computeCenter();
+	}
+
+	public void add(Unit u) {
+		this.unitList.add(u);
+		u.ai.setSquad(this);
+		computeCenter();
+	}
+
+	public void merge(Squad s) {
+		for (Unit u : s.unitList) {
+			add(u);
+		}
+		computeCenter();
+	}
+	
+	public void computeCenter () {
+		int amountX;
+		int amountY;
+		
+		amountX = 0;
+		amountY = 0;
+		for (Unit u : this.unitList){
+			amountX += u.getPosition().getX();
+			amountY += u.getPosition().getY();
+		}
+		this.center.setX((int) amountX/this.unitList.size());
+		this.center.setY((int) amountY/this.unitList.size());
+	}
+}
