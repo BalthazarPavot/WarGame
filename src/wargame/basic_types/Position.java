cee9fba7954ff503861b951c055c592a24379274
tilito <pelegrinromain@gmail.com>
2016-11-27 04:34:48 +0100

    add setter of X and Y. Format

--- src/wargame/basic_types/Position.java
+++ src/wargame/basic_types/Position.java
@@ -48,6 +48,14 @@ public class Position implements Comparable<Object> {
 		return y;
 	}
 
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 	/**
 	 * @param x
 	 * @param y
@@ -100,82 +108,20 @@ public class Position implements Comparable<Object> {
 	/**
 	 * @return All the positions reachable giving the amount of moving points.
 	 */
-	public ArrayList<Position> getReachableNeighbor(int movePoints) {/*
-																	 * ArrayList<
-																	 * Position>
-																	 * neighbor;
-																	 * 
-																	 * neighbor
-																	 * = new
-																	 * ArrayList
-																	 * <
-																	 * Position>
-																	 * ();
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x, y -
-																	 * Map
-																	 * .squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y -
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y +
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x, y +
-																	 * Map
-																	 * .squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y +
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y -
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * return
-																	 * neighbor;
-																	 */
+	public ArrayList<Position> getReachableNeighbor(int movePoints) {
+		/*
+		 * ArrayList< Position> neighbor;
+		 * 
+		 * neighbor = new ArrayList < Position> (); neighbor .add(new Position
+		 * (x, y - Map .squareHeight )); neighbor .add(new Position (x + Map.
+		 * squareWidth , y - Map. squareHeight )); neighbor .add(new Position (x
+		 * + Map. squareWidth , y)); neighbor .add(new Position (x + Map.
+		 * squareWidth , y + Map. squareHeight )); neighbor .add(new Position
+		 * (x, y + Map .squareHeight )); neighbor .add(new Position (x - Map.
+		 * squareWidth , y + Map. squareHeight )); neighbor .add(new Position (x
+		 * - Map. squareWidth , y)); neighbor .add(new Position (x - Map.
+		 * squareWidth , y - Map. squareHeight )); return neighbor;
+		 */
 		return new ArrayList<Position>();
 	}
 
@@ -183,82 +129,20 @@ public class Position implements Comparable<Object> {
 	 * @return All the positions targetable by a character, giving his sight
 	 *         line.
 	 */
-	public ArrayList<Position> getTargetableNeighbor(int sightLigne) {/*
-																	 * ArrayList<
-																	 * Position>
-																	 * neighbor;
-																	 * 
-																	 * neighbor
-																	 * = new
-																	 * ArrayList
-																	 * <
-																	 * Position>
-																	 * ();
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x, y -
-																	 * Map
-																	 * .squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y -
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x + Map.
-																	 * squareWidth
-																	 * , y +
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x, y +
-																	 * Map
-																	 * .squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y +
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y));
-																	 * neighbor
-																	 * .add(new
-																	 * Position
-																	 * (x - Map.
-																	 * squareWidth
-																	 * , y -
-																	 * Map.
-																	 * squareHeight
-																	 * ));
-																	 * return
-																	 * neighbor;
-																	 */
+	public ArrayList<Position> getTargetableNeighbor(int sightLigne) {
+		/*
+		 * ArrayList< Position> neighbor;
+		 * 
+		 * neighbor = new ArrayList < Position> (); neighbor .add(new Position
+		 * (x, y - Map .squareHeight )); neighbor .add(new Position (x + Map.
+		 * squareWidth , y - Map. squareHeight )); neighbor .add(new Position (x
+		 * + Map. squareWidth , y)); neighbor .add(new Position (x + Map.
+		 * squareWidth , y + Map. squareHeight )); neighbor .add(new Position
+		 * (x, y + Map .squareHeight )); neighbor .add(new Position (x - Map.
+		 * squareWidth , y + Map. squareHeight )); neighbor .add(new Position (x
+		 * - Map. squareWidth , y)); neighbor .add(new Position (x - Map.
+		 * squareWidth , y - Map. squareHeight )); return neighbor;
+		 */
 		return new ArrayList<Position>();
 	}
 
