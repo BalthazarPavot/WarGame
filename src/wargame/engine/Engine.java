ac1aac6e2c3c9f7049317b578ea3e82e42a71393
balto <balto@lain>
2016-11-27 22:38:48 +0100

    Better turn based game play.

--- src/wargame/engine/Engine.java
+++ src/wargame/engine/Engine.java
@@ -1,10 +1,9 @@
 
 package wargame.engine;
 
-import java.awt.Graphics;
 import java.util.ArrayList;
 
-
+import wargame.GameContext;
 import wargame.basic_types.Position;
 import wargame.map.Map;
 import wargame.unit.Unit;
@@ -12,44 +11,50 @@ import wargame.widgets.AnimationWidget;
 import wargame.widgets.MapWidget;
 import wargame.widgets.SidePanel;
 
-
+/**
+ * The engine handle the actions done by the player to make it's characters to attack, move etc. It updates
+ * the map widget and the side panel to make them display each action.
+ * 
+ * @author Balthazar Pavot
+ *
+ */
 public class Engine {
 
-	ArrayList<Unit> playerUnits;
-	ArrayList<Unit> ennemyUnits;
-	private MapWidget mapWidget;
-	private SidePanel sidePanel;
+	ArrayList<Unit> playerUnits = null;
+	ArrayList<Unit> ennemyUnits = null;
+	private MapWidget mapWidget = null;
+	private SidePanel sidePanel = null;
 	private Map map = null;
 	private Unit selectedAllie = null;
 	private Unit selectedEnnemy = null;
 	public ArrayList<Position> currentPath = null;
 	private boolean autoGameMode = false;
-	private AnimationWidget currentAnimation = null;
+	private GameContext context = null;
 
-	public Engine(Map map, MapWidget mapWidget, SidePanel sidePanel) {
-		this.map = map;
+	public Engine(GameContext context, MapWidget mapWidget, SidePanel sidePanel) {
+		this.map = context.getMap();
 		this.mapWidget = mapWidget;
 		this.sidePanel = sidePanel;
+		this.context = context;
 		initPlayerUnits();
 		initEnnemyUnits();
 	}
 
-	public void initPlayerUnits() {
-		playerUnits = new ArrayList<Unit>();
-		playerUnits.add(new Unit(map.getAlliePopArea()));
-		playerUnits.get(0).setMove(map.pathByWalking(map.getAlliePopArea(), map.getEnnemyPopArea()));
-	}
-
-	public void initEnnemyUnits() {
-		ennemyUnits = new ArrayList<Unit>();
-	}
-
+	/**
+	 * Dispatch all action to the given characters, update the fog and pass to the next turn.
+	 */
 	public void nextTurn() {
-		for (Unit ennemy : ennemyUnits)
-			ennemy.play(playerUnits, ennemyUnits, map);
-		for (Unit allie : playerUnits)
-			allie.play(playerUnits, ennemyUnits, map);
+		for (Unit allie : playerUnits) {
+			displayAction(allie.play(playerUnits, ennemyUnits, map), allie);
+			allie.hasPlayed = false;
+		}
+		for (Unit ennemy : ennemyUnits) {
+			displayAction(ennemy.play(playerUnits, ennemyUnits, map), ennemy);
+			ennemy.hasPlayed = false;
+		}
 		updateFog(playerUnits);
+		selectAllie(null);
+		selectEnnemy(null);
 	}
 
 	/**
@@ -59,6 +64,113 @@ public class Engine {
 		updateFog(playerUnits);
 	}
 
+	/**
+	 * 
+	 * @param position
+	 */
+	public void mapMotioned(Position position) {
+		if (selectedAllie != null)
+			currentPath = selectedAllie.canMove(map, position);
+	}
+
+	/**
+	 * Select/unselect units when the map is clicked, on execute actions.
+	 * 
+	 * @param position
+	 */
+	public void mapLeftClicked(Position position) {
+		position = mapWidget.getInGamePosition(position);
+		for (Unit unit : playerUnits) {
+			if (unit.isClicked(position)) {
+				if (selectedAllie != null)
+					allieActOnAllie(unit);
+				else {
+					selectAllie(unit);
+				}
+				return;
+			}
+		}
+		for (Unit unit : ennemyUnits) {
+			if (unit.isClicked(position)) {
+				if (selectedAllie != null)
+					allieActOnEnnemy(unit);
+				else {
+					selectedAllie = null;
+					selectEnnemy(unit);
+				}
+				return;
+			}
+		}
+		if (selectedAllie != null) {
+			allieMoveTo(position);
+			return;
+		}
+		// selectAllie (null);
+		// selectEnnemy(null);
+	}
+
+	public void mapRightClicked(Position mapPosition) {
+		selectAllie(null);
+		selectEnnemy(null);
+	}
+
+	private void selectAllie(Unit unit) {
+		selectedAllie = unit;
+		selectedEnnemy = null;
+		mapWidget.interfaceWidget
+				.setPathToDisplay(selectedAllie == null ? null : selectedAllie.getPathToEnd());
+	}
+
+	/**
+	 * set the given unit selected
+	 * 
+	 * @param selectedEnnemy
+	 */
+	public void selectEnnemy(Unit selectedEnnemy) {
+		selectAllie(null);
+		this.selectedEnnemy = selectedEnnemy;
+	}
+
+	/**
+	 * Create and initialise the units of the player.
+	 */
+	private void initPlayerUnits() {
+		playerUnits = new ArrayList<Unit>();
+		playerUnits.add(new Unit(map.getAlliePopArea(), context.getSpriteHandler()));
+		playerUnits.get(0).setMove(map.pathByWalking(map.getAlliePopArea(), map.getEnnemyPopArea()));
+	}
+
+	/**
+	 * Create and initialise the enemy units.
+	 */
+	private void initEnnemyUnits() {
+		ennemyUnits = new ArrayList<Unit>();
+	}
+
+	/**
+	 * Display the action of a unit in function of its action code.
+	 * 
+	 * @param action
+	 * @param unit
+	 */
+	private void displayAction(int action, Unit unit) {
+		switch (action) {
+		case Unit.MOVE_ACTION:
+			setAnimation(unit.getCurrentWalkAnimation(), unit.getPreviousPosition());
+			break;
+		case Unit.ATTACK_ACTION:
+			break;
+		case Unit.NO_ACTION:
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * For each unit update it's vision.
+	 * 
+	 * @param units
+	 */
 	private void updateFog(ArrayList<Unit> units) {
 		mapWidget.freeFog();
 		for (Unit unit : units) {
@@ -76,45 +188,11 @@ public class Engine {
 		sidePanel.buildFog(mapWidget);
 	}
 
-	public void mapMotioned(Position position) {
-		if (selectedAllie != null)
-			currentPath = selectedAllie.canMove(map, position);
-	}
-
 	/**
-	 * Select/unselect units when the map is clicked, on execute actions.
+	 * make the selected unit to move to the given point.
 	 * 
 	 * @param position
 	 */
-	public void mapClicked(Position position) {
-		for (Unit unit : playerUnits)
-			if (unit.isClicked(position)) {
-				if (selectedAllie != null)
-					allieActOnAllie(unit);
-				else {
-					selectedAllie = unit;
-					setSelectedEnnemy(null);
-				}
-				return;
-			}
-		for (Unit unit : ennemyUnits)
-			if (unit.isClicked(position)) {
-				if (selectedAllie != null)
-					allieActOnEnnemy(unit);
-				else {
-					selectedAllie = null;
-					setSelectedEnnemy(unit);
-				}
-				return;
-			}
-		if (selectedAllie != null) {
-			allieMoveTo(position);
-			return;
-		}
-		selectedAllie = null;
-		setSelectedEnnemy(null);
-	}
-
 	private void allieMoveTo(Position position) {
 		if (currentPath != null)
 			selectedAllie.setMove(currentPath);
@@ -151,10 +229,11 @@ public class Engine {
 		return selectedEnnemy;
 	}
 
-	public void setSelectedEnnemy(Unit selectedEnnemy) {
-		this.selectedEnnemy = selectedEnnemy;
-	}
-
+	/**
+	 * return if the game is playing automatically
+	 * 
+	 * @return
+	 */
 	public boolean isAutoGame() {
 		return autoGameMode;
 	}
@@ -196,8 +275,8 @@ public class Engine {
 	 * 
 	 * @param animation
 	 */
-	public void setAnimation (AnimationWidget animation) {
-		setAnimation (animation, 0, 0) ;
+	public void setAnimation(AnimationWidget animation) {
+		setAnimation(animation, 0, 0);
 	}
 
 	/**
@@ -206,24 +285,22 @@ public class Engine {
 	 * @param animation
 	 * @param position
 	 */
-	public void setAnimation (AnimationWidget animation, Position position) {
+	public void setAnimation(AnimationWidget animation, Position position) {
 		setAnimation(animation, position.getX(), position.getY());
 	}
 
-	public void setAnimation (AnimationWidget animation, int x, int y) {
-		currentAnimation = animation ;
-		currentAnimation.move(x, y);
+	/**
+	 * make the map display the animation at the given position
+	 * 
+	 * @param animation
+	 * @param x
+	 * @param y
+	 */
+	public void setAnimation(AnimationWidget animation, int x, int y) {
+		if (animation != null) {
+			animation.move(x, y);
+			animation.replay();
+		}
+		mapWidget.interfaceWidget.setCurrentAnimation(animation);
 	}
-
-	public void displayAnimations(Graphics g) {
-		if (currentAnimation == null)
-			return;
-		if (!currentAnimation.paintComponent (g))
-			currentAnimation = null ;
-	}
-
-	public boolean inAnimationLoop() {
-		return currentAnimation != null ;
-	}
-
 }
