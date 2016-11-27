f702ec6994443ec718f5176ee3302816ccfaf12b
slarzack <bennacer.kamil@gmail.com>
2016-11-27 11:33:01 +0100

    bug correction

--- src/UnitPackage/Healer.java
+++ src/UnitPackage/Healer.java
@@ -1,12 +1,14 @@
 package UnitPackage;
 
+import wargame.basic_types.Position;
+
 public class Healer extends Terrestre {
 
 
 	
 	Healer(int pvmax, double atkSlaching, double defSlaching, double atkBlunt, double defBlunt, double atkPercing,
-			double defPercing, double atkMagic, double defMagic) {
-		super(pvmax, atkSlaching, defSlaching, atkBlunt, defBlunt, atkPercing, defPercing, atkMagic, defMagic);
+			double defPercing, double atkMagic, double defMagic,Position position) {
+		super(pvmax, atkSlaching, defSlaching, atkBlunt, defBlunt, atkPercing, defPercing, atkMagic, defMagic, position);
 		// TODO Auto-generated constructor stub
 	}
 
