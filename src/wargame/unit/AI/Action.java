6c5046df479a2316bf3061310d3163042b9140f6
tilito <pelegrinromain@gmail.com>
2016-12-07 23:57:54 +0100

    remove getters and setters

--- src/wargame/unit/AI/Action.java
+++ src/wargame/unit/AI/Action.java
@@ -1,50 +1,17 @@
 package wargame.unit.AI;
 
+import java.util.ArrayList;
+
 import wargame.basic_types.Position;
 
 public class Action {
 
 	/* Enumerations */
 	public enum operation {
-		ATTACK, HEAL, WAIT, FIRE, UNDEF ;
+		MOVE, ATTACK, HEAL, WAIT;
 	}
 	
-	/* Attribut of the class */
-	private Position positionFinal;
-	private Position targetPosition;
-	private operation ope;
-
-	/* Getters and setters */
-	public Position getPositionFinal() {
-		return positionFinal;
-	}
-
-	public void setPositionFinal(Position positionFinal) {
-		this.positionFinal = positionFinal;
-	}
-
-	public Position getTargetPosition() {
-		return targetPosition;
-	}
-
-	public void setTargetPosition(Position targetPosition) {
-		this.targetPosition = targetPosition;
-	}
-
-	public operation getOpe() {
-		return ope;
-	}
-
-	public void setOpe(operation ope) {
-		this.ope = ope;
-	}
-	
-	/* Methods */
-	public void initialize(){
-		this.ope = operation.UNDEF;
-		this.positionFinal = null;
-		this.targetPosition = null;
-	}
-
-
+	/* Attribute of the class */
+	public ArrayList<Position> position;
+	public operation ope;
 }
